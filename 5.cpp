41906693ab448280b87cdfe611de093cd196ccd4
failedcoder12 <divyanshverma12@gmail.com>
2018-11-30 17:35:58 +0530

    1 to 5

--- 5.cpp
+++ 5.cpp
@@ -1,8 +1,25 @@
+// https://www.codechef.com/problems/RRPLAYER
+
 #include<bits/stdc++.h>
 using namespace std;
 #define FastRead ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define ll long long int
 
+// COUPON COLLECTION PROBLEM 
+// n*Hn Harmonic Series
+
 int main(){
 	FastRead;
+
+	ll t;
+	cin>>t;
+
+	while(t--){
+		float n;
+		cin>>n;
+
+		float ans = n*log(n) + n*(0.577216) + 0.50;
+
+		printf("%.2f\n",ans);
+	}
 }
